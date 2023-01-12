e48ef2c8175503e1dfda736ac11d10ae4d58b073
vutheanh1611 <vutheanh121216112000@gmail.com>
2023-01-12 12:19:48 +0700

    assigmenthouse

---
+++ house-main/house-main/ConsoleApp3/HaNoiHouse.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using house.cm;
+namespace house.hanoi
+{
+    public class HaNoiHouse : House
+    {
+        private string tenQuan;
+        public string TenQuan { get; set; }
+
+        public HaNoiHouse() { }
+
+        public HaNoiHouse(string SoNha, string DiaChi, string LoaiNha, string tenQuan) : base(SoNha, DiaChi, LoaiNha)
+        {
+            this.tenQuan = tenQuan;
+        }
+
+        public override void input()
+        {
+            base.input();
+        }
+
+        public override void display()
+        {
+            base.display();
+        }
+
+    }
+    internal class ManagerHouse
+    {
+        public ManagerHouse() { }
+    }
+}
---
+++ house-main/house-main/ConsoleApp3/House.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace house.cm
+{
+    public class House : IHouse
+    {
+        private string soNha;
+        private string diaChi;
+        private string loaiNha;
+
+        public string SoNha
+        {
+            get { return soNha; }
+            set { soNha = value; }
+        }
+        public string DiaChi { get; set; }
+        public string LoaiNha { get; set; }
+
+
+
+        public House() { }
+
+        public House(string soNha, string diaChi, string loaiNha)
+        {
+            this.soNha = soNha;
+            this.diaChi = diaChi;
+            this.loaiNha = loaiNha;
+        }
+
+        public virtual void display()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void input()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IHouse
+    {
+        public abstract void input();
+        public abstract void display();
+    }
+}
